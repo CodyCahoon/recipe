6df1648bdcd2b0d2da71eeb6065c23652ca0d4ed
Cody Cahoon <cody.cahoon@levvel.io>
2020-10-16 22:51:01 -0400

    feature: Adds GraphQL and a simple User Entity

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
+import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { UserResolver } from './resolver/user';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
-  const connection = await createConnection();
+  await createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'recipe',
+    synchronize: true,
+    logging: true,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+  });
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+      validate: false,
+    }),
+    resolvers: [UserResolver] as any[],
+  });
+  const app = express();
+
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+  );
 };
 
 main();
